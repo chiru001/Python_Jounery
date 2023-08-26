98bf2dd659ed0ee574d1ac5a8fc7a1669631eafc
chiranjeevi <chirucenaa@gmail.com>
2023-08-27 00:46:29 +0530

    Created_Optional_pyfile

---
+++ 6_Optional.py
@@ -0,0 +1,19 @@
+#! We have an extra datatype which we didn't talk about, and its called "Complex"
+#! Complex Data_type
+
+#! We know that integres and float will be stored as binay in memory as 1's & 0's
+
+#! we actually have a function to convert integer to binay that is "BIN"
+
+bin(5) #!output is ? hahahh
+print(bin(5)) #!output is 0b101
+
+#! here python will represent as B if you check binary for 5 is 101 so we have 101 before 0b, where B reoresents as Binary.
+
+#! So now we need to retrive the binay number to decimal number ?
+
+print(int('0b101', 2)) #!output is 5
+
+#! 2 is base 2
+
+
