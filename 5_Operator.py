a6646e8a414f9e17e35e862aad189e68a4687a7e
chiranjeevi <chirucenaa@gmail.com>
2023-08-27 00:35:30 +0530

    Created_Operator_pyfile

---
+++ 5_Operator.py
@@ -0,0 +1,29 @@
+# Operator Precedence
+#! What does Precedence means? 
+#! example : 20 + 3 * 4 here it means different operators have precedence over differen ones.
+#! That is we mutiply this first equals to 12 and then 20 gets added to 12.
+#! 20 + 3 * 4
+#! 20 + 12
+#! 32
+print (20 + 3 * 4)
+#! Because the python interpreter is going to follow a basic set of rules.
+#! 20 - 3 * 4 
+#! 20 - 12 = 8
+print (20 - 3 * 4)
+
+#!++++++++++++++++++++++++++++++++Order++++++++++++++++++++++++++++++++++++++++++
+#! ()
+#! **
+#! * /
+#! + , -
+
+print ((20 - 3) + 2 **2)
+
+#!Here first brackets so 20 -3 = 17 & 2**2= 4 last last "+"" 17 + 4 =21
+
+
+
+
+
+
+
