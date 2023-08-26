233970ea9c99d57a3219c30c1b296d89ee2ce879
chiranjeevi <chirucenaa@gmail.com>
2023-08-27 01:00:33 +0530

    Created_EXPRESSIONVSSTATMENT_pyfile

---
+++ 8_Expressionvsstatment.py
@@ -0,0 +1,12 @@
+#! What is the difference between expression vs statement?
+
+#! Assume below code and in that expression is iq / 5. Its a piece of code that produces a value
+iq = 100
+user_age = iq / 5 #! output is 20.0
+print(user_age)
+
+#! here user_age - iq / 5 the whole line is called as "STATMENT" iq = 100 this is also a another statment.
+
+
+#! EXPRESSION = PRODUCE A VALUE 
+#! STATEMENT = DATA WHICH WE GIVE TO PRODUCE THAT VALUE IS CALLED STATMENT.
