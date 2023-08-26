fcd01ee1c6aaa2aff40c02508232866000f1b626
chiranjeevi <chirucenaa@gmail.com>
2023-08-27 00:53:14 +0530

    Created_Variables_pyfile

---
+++ 7_variables.py
@@ -0,0 +1,20 @@
+#! Best pratices for variables
+
+#! snake_case
+#! Start with lowercase or underscore
+#! Letters, numbers, underscores
+#! Case Sensitive
+#! Don't overwrite keywords.
+
+a,b,c = 1,2,3
+print(a)
+print(b)
+print(c)
+
+Name = "Chiranjeevi Dronamraju"
+Age = 23
+Company = "testabcd"
+
+print(Name)
+print(Age)
+print(Company)
