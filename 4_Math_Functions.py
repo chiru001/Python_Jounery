cc2537633bc64ee33d49853702abf123dedb4ffe
chiranjeevi <chirucenaa@gmail.com>
2023-08-27 00:23:44 +0530

    Created_MathFunctons_pyfile

---
+++ 4_Math_Functions.py
@@ -0,0 +1,14 @@
+#! We are starting to learn about datatypes and the actions that we can perform on them
+#! In most programming languages, these actions are called functions. print is function and type is a function which are action that we performed on the datatypes.
+
+#! Here we have a specific Math-Functions such as "round"
+print(round(3.9)) #! output will be 4 it will just round the number
+print(round(99.5)) #! output will be 100
+
+#! So we also have another Math-Funcion that is built into python is soemthing called as "abs".
+#! abs will return the absolute value of the argument.means "NO NEGATIVE NUMBER"
+print(abs(-20)) #! output will be 20
+print(abs(-1000)) #! output will be 1000
+
+#! there are so many Math-Functions which you can see from below link
+#! Link: programiz.com/python-programming/modules/math
