cb381dfc37d421172941f0558ee7ea7f4c7a766a
chiranjeevi <chirucenaa@gmail.com>
2023-08-27 13:00:46 +0530

    created_list_pyfile

---
+++ 19_Lists.py
@@ -0,0 +1,25 @@
+#! List
+#! W can have any collection of iteams that we want 
+List_demo = [1,2,3,4,5,6,7]
+List_demo_two = ['a', 'b', 'c','d','e']
+
+#! we can also mix a match.
+List_demo_mix = [1,2,'a',3,'b',4, True]
+
+#!In other programming language we have same same functionality called arrays
+#! In python lists are a form of array. later we talk different b/w array and lists.
+
+#! Data Structure is a very important concepts in programming languages. It's a way for us to
+#! Organized info and data into, lets say a folder or a cupboardd or a box.
+
+#! Thekey here is that the square brackets allow us to contain information and data like strings, integers, booleans etc.
+
+#! example as amazon cart
+
+amazon_cart = ['notebooks', 'sunglasses']  #!so in cart we have 2 iteam now now to access it?
+
+print(amazon_cart[0])
+print(amazon_cart[1])
+#print(amazon_cart[2]) #! What about this?? there is no iteam 2 so it will shows error
+
+
