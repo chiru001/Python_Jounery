1a907af7e3d5b06cc05fe7df24e4c1b93299ec31
chiranjeevi <chirucenaa@gmail.com>
2023-08-27 10:58:46 +0530

    created_boolean_pyfile

---
+++ 16_Booleans.py
@@ -0,0 +1,10 @@
+#! Booleans
+
+#! In python Booleans are called as bool there is only 2 options true or false
+
+print(bool(1))
+print(bool(0))
+print(bool(4))
+print(bool(5))
+
+#! we will discuse later in conditions
