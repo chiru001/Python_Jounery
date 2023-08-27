b286736e9c2d41632653b523ff48acf3c6915056
chiranjeevi <chirucenaa@gmail.com>
2023-08-27 18:40:05 +0530

    created_none_pyfile

---
+++ 27_None.py
@@ -0,0 +1,9 @@
+#! AS we know null or noen is empty 
+#! But why it is useful?
+
+#! For now it hard to really conceptulize it .
+
+#! asume that your are playing pubg and there will a lots of weapons, at beginning you 
+#! don't have weapons in that cases we will use none
+
+Weapons = None
